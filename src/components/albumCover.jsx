42fddffc58b1a12fd77cb93a62eb4af98e742e39
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-12-10 17:10:01 -0300

    photos stacked

--- src/components/albumCover.jsx
+++ src/components/albumCover.jsx
@@ -2,16 +2,39 @@ import React from 'react';
 
 function AlbumCover(props) {
   return (
-    <div className="max-w-xs p-2 self-center">
+    <div className="w-full p-2 self-center">
       <div className="flex flex-row rounded-lg">
-        <div className="group relative w-full h-full shadow-md rounded-lg cursor-pointer">
+        <div
+          className="
+            group
+            relative
+            w-full
+            h-full
+            shadow-md
+            rounded-lg
+            cursor-pointer
+          "
+        >
           <img
            src={props.imgUrl}
-           className="shadow-base rounded-lg" alt="album cover"
+           className="w-full shadow-base rounded-lg" alt="album cover"
           />
           <div
-            className="absolute inset-x-0 bottom-0 text-gray-100 p-3 rounded-b-lg text-center lg:hidden lg:group-hover:block"
-            style={{backgroundImage: "linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5), transparent)"}}>
+            className="
+              absolute
+              inset-x-0
+              bottom-0
+              text-gray-100
+              p-3
+              rounded-b-lg
+              text-center
+              hidden
+              group-hover:block
+            "
+            style={{
+              backgroundImage: "linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5), transparent)"
+            }}
+          >
             <small className="text-xs font-hairline">
               MODELO
             </small>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import moment from 'moment';
+import _ from 'lodash';
 
 import AlbumCover from './albumCover';
 
@@ -10,9 +11,9 @@ function Home() {
   useEffect(() => {
     const getCovers = async () => {
       const result = await await axios(
-      'http://localhost:5000/',
+      'http://192.168.0.10:5000/',
     );
-      setCovers(JSON.parse(result.request.response));
+      setCovers(_.values(JSON.parse(result.request.response)));
     };
     getCovers();
   });
@@ -21,17 +22,62 @@ function Home() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col sm:flex-col md:flex-row self-center">
-        {
-          Object.values(covers).map(cover => (
+      <div
+        className="
+          flex
+          flex-col
+          w-full md:max-w-2xl lg:max-w-3xl xl:max-w-4xl
+          md:mx-auto
+        "
+      >
+        <div className="w-3/4 md:w-7/12 md:-mb-24 lg:w-9/12 lg:-mb-64">
+          {
             <AlbumCover
-              key={cover.cover_url}
-              imgUrl={cover.cover_url}
-              modelName={cover.model_name}
-              date={moment(new Date(cover.book_date)).format('DD MMM. YYYY')}
+              imgUrl={covers[0].cover_url}
+              modelName={covers[0].model_name}
+              date={
+                moment(new Date(covers[0].book_date)).format('DD MMM. YYYY')
+              }
             />
-          ))
-        }
+          }
+        </div>
+        <div
+          className="
+            w-3/4 md:w-2/3 lg:w-1/2
+            -mt-40 md:-mt-48
+            -mb-6 lg:-mb-1
+            self-end
+            z-20
+          "
+        >
+          {
+            <AlbumCover
+              imgUrl={covers[1].cover_url}
+              modelName={covers[1].model_name}
+              date={
+                moment(new Date(covers[1].book_date)).format('DD MMM. YYYY')
+              }
+            />
+          }
+        </div>
+        <div
+          className="
+            w-3/4 md:w-3/4 lg:w-7/12
+            -mt-24 md:-mt-64
+            md:ml-8
+            z-10
+          "
+        >
+          {
+            <AlbumCover
+              imgUrl={covers[2].cover_url}
+              modelName={covers[2].model_name}
+              date={
+                moment(new Date(covers[2].book_date)).format('DD MMM. YYYY')
+              }
+            />
+          }
+        </div>
       </div>
       <div className="flex flex-col w-full justify-center bg-blue-100 mt-8">
         <div className="max-w-5xl self-center">
@@ -47,12 +93,24 @@ function Home() {
           <div className="flex justify-center pb-6 flex-col md:flex-row">
             <div className="self-center pb-6">
               <img
-                className="object-cover rounded-full h-40 w-40 md:mr-6" alt="Mateo"
+                className="object-cover rounded-full h-40 w-40 md:mr-6"
                 src="https://pro2-bar-s3-cdn-cf3.myportfolio.com/02430f6d-af4e-45d1-9a86-bd4dd91db1f7/416cb9dc-d5a4-4bfb-a9dc-d30938854c31_rwc_0x22x4015x5989x4096.jpg?h=a88666c59b26eafbfc33d277d5a5002d"
-                />
+                alt="Mateo"
+              />
             </div>
             <div className="relative">
-              <span className="absolute font-serif font-black text-6xl text-blue-800 left-0 top-0">❝</span>
+              <span
+                className="
+                  absolute
+                  font-serif
+                  font-black
+                  text-6xl
+                  text-blue-800
+                  left-0
+                  top-0"
+              >
+                ❝
+              </span>
               <p
                 className="
                 text-center font-medium leading-relaxed
@@ -69,7 +127,18 @@ function Home() {
                 del 2017. Desde ese momento es que estoy
                 trabajando como fotógrafo y estoy muy contento
               </p>
-              <span className="absolute font-serif font-black text-6xl text-blue-800 right-0 bottom-0">❞</span>
+              <span
+                className="
+                  absolute
+                  font-serif
+                  font-black
+                  text-6xl
+                  text-blue-800
+                  right-0
+                  bottom-0"
+                >
+                  ❞
+                </span>
             </div>
           </div>
         </div>
