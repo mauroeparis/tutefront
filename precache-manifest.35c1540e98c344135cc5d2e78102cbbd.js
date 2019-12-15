cecd0356eec2429821da4b9f308d7546477973d9
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-12-15 11:36:18 -0300

    Updates

---
+++ precache-manifest.35c1540e98c344135cc5d2e78102cbbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a9635baa547da51e673a890280cb0f",
+    "url": "/tutefront/index.html"
+  },
+  {
+    "revision": "a025b592894045aaad26",
+    "url": "/tutefront/static/css/main.db419c65.chunk.css"
+  },
+  {
+    "revision": "9f1cc7a4dba53ec5ab1a",
+    "url": "/tutefront/static/js/2.e95ac09e.chunk.js"
+  },
+  {
+    "revision": "a025b592894045aaad26",
+    "url": "/tutefront/static/js/main.284ef30f.chunk.js"
+  },
+  {
+    "revision": "16e3ef3bf7f228a323cf",
+    "url": "/tutefront/static/js/runtime~main.3c46496b.js"
+  },
+  {
+    "revision": "b32635e1859886eb62c7bf2489c3b699",
+    "url": "/tutefront/static/media/chat.b32635e1.svg"
+  },
+  {
+    "revision": "0186846f4c034eb144a45334211d478e",
+    "url": "/tutefront/static/media/dots.0186846f.svg"
+  },
+  {
+    "revision": "228c758c8ab7ca9b893f942ec46150b8",
+    "url": "/tutefront/static/media/instagram.228c758c.svg"
+  }
+]);
