cd04c58664ab60a7b3d669b18169e2680ec8b2ca
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2020-02-16 23:22:45 -0300

    add navbar simple hover animations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion"
+
 
 import { ReactComponent as InstaLogo } from '../icons/instagram.svg';
 import { ReactComponent as WppLogo } from '../icons/whatsapp.svg';
@@ -36,18 +38,33 @@ function Navbar() {
 
       </div>
       <div className="flex flex-row bg-white py-2 pr-1">
-        <a className="self-center" href="https://www.instagram.com/matuteph/">
-          <InstaLogo className="px-3 h-6 w-12"/>
-        </a>
-        <a className="self-center" href="https://api.whatsapp.com/send?phone=+5493512016383">
-          <WppLogo className="px-3 h-6 w-12"/>
-        </a>
-        <Link
-          to="/gallery"
-          className="self-center"
+        <motion.div
+          whileHover={{ scale: 1.2, color:"#C13584" }}
+          whileTap={{ scale: 0.8, borderRadius: "100%" }}
         >
-          <ImagesLogo className="px-3 h-6 w-12"/>
-        </Link>
+          <a className="self-center" href="https://www.instagram.com/matuteph/">
+            <InstaLogo className="px-3 h-6 w-12"/>
+          </a>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.2, color:"#25D366" }}
+          whileTap={{ scale: 0.8, borderRadius: "100%" }}
+        >
+          <a className="self-center" href="https://api.whatsapp.com/send?phone=+5493512016383">
+            <WppLogo className="px-3 h-6 w-12" />
+          </a>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.2, color: "#34B7F1" }}
+          whileTap={{ scale: 0.8, borderRadius: "100%" }}
+        >
+          <Link
+            to="/gallery"
+            className="self-center"
+          >
+            <ImagesLogo className="px-3 h-6 w-12" />
+          </Link>
+        </motion.div>
         {token.token ?
           <button
             onClick={() => logout()}
